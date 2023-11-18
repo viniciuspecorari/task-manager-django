e07be234b32cf2fdffb03ecfaf18e79267b4cb09
Vinicius Pecorari <ovinicius.pecorari@gmail.com>
2023-11-18 13:07:26 -0300

    Configurando nova view

--- todos/views.py
+++ todos/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
+from .models import Todo
 
 
-def home(request):
-    return render(request, "todos/home.html")
+def todo_list(request):
+    todos = Todo.objects.all()
+    return render(request, "todos/todo_list.html", {"todos": todos})
