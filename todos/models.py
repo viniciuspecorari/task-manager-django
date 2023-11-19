b8b19bf8a79533a3c9942df437cf2dc6f959c500
Vinicius Pecorari <ovinicius.pecorari@gmail.com>
2023-11-19 13:22:36 -0300

    Finalização

--- todos/models.py
+++ todos/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from datetime import date
 # Create your models here.
 
 
@@ -11,3 +11,11 @@ class Todo(models.Model):
     deadline = models.DateField(
         verbose_name="Data de entrega", null=False, blank=False)
     finished_at = models.DateField(null=True)
+
+    class Meta:
+        ordering = ["deadline"]
+
+    def mark_has_complete(self):
+        if not self.finished_at:
+            self.finished_at = date.today()
+            self.save()
--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,7 @@
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
 from .models import Todo
 from django.urls import reverse_lazy
-
+from django.shortcuts import get_object_or_404, redirect
 
 class TodoListView(ListView):
     model = Todo
@@ -22,3 +22,11 @@ class TodoUpdateView(UpdateView):
 class TodoDeleteView(DeleteView):
     model = Todo
     success_url = reverse_lazy("todo_list")
+
+
+class TodoCompleteView(View):
+
+    def get(self, request, pk):
+        todo = get_object_or_404(Todo, pk=pk)
+        todo.mark_has_complete()
+        return redirect("todo_list")
